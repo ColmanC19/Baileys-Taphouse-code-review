d97626d3d2aeb0298ef46c9ed118036510d2013a
Colman Currie <curriecolman@gmail.com>
2020-02-14 12:05:09 -0800

    add taps page, style it up

--- Desktop/week-one/src/components/HomeBody.jsx
+++ Desktop/week-one/src/components/HomeBody.jsx
@@ -3,19 +3,27 @@ import React from "react";
 
 const HomeBodycss = {
   display: "inlineBlock",
+  gridTemplateColumns: "1fr 1fr 1fr",
   border: "solid",
-  padding: "20px",
-  margin: "5px",
+  padding: "30px",
+  margin: "Auto",
   width: "500px",
   height: "150px"
 }
 function HomeBody(){
   return(
+    <div>
     <div style ={HomeBodycss}>
     <h1>Welcome to Baileys Taphouse</h1>
     <h2>An experience for the craft brew enthusiast. Bring your friends, parents, spouse or your pooch. No Minors.</h2>
     </div>
-    
+    <div style ={HomeBodycss}>
+    <h1>Plan your next event here!</h1>
+    <h2>Bachelor/Bachelorette parties, weddings, meetups, office outings or any reason to get a large group of friends together! Enter your email below and we'll get in touch with you.</h2>
+
+    </div>
+    </div>
+
   )
 }
 
