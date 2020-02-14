38eba214020b594d069ae27185d6577577136610
Colman Currie <curriecolman@gmail.com>
2020-02-14 10:22:54 -0800

    styling of main page

--- Desktop/week-one/src/App.js
+++ Desktop/week-one/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header.jsx'
+import HomeBody from './components/HomeBody.jsx'
 
 function App() {
   return (
     <div className="App">
     <Header />
     <h1 className ="Bailey"> Bailey's Taphouse</h1>
-
+    <HomeBody />
     </div>
   );
 }
---
+++ Desktop/week-one/src/components/HomeBody.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+const HomeBodycss = {
+  display: "inlineBlock",
+  border: "solid",
+  padding: "20px",
+  margin: "5px",
+  width: "250px",
+  height: "400px"
+}
+function HomeBody(){
+  return(
+    <div>
+<h1>Welcome to Baileys Taphouse</h1>
+<h2>An experience for the craft brew enthusiast. Bring your friends, parents, spouse or your pooch. No Minors.</h2>
+</div>
+  )
+}
+
+export default HomeBody
